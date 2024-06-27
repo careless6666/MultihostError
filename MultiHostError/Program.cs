42e89444624395c540f4629247716a798bb96c2f
kupes <kupes@kupes.com>
2024-06-27 13:34:59 +0300

    first commit

---
+++ MultiHostError/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Npgsql;
+using Serilog;
+
+Console.WriteLine("Hello, World!");
+
+var dataSourceBuilder = new NpgsqlDataSourceBuilder("User ID=postgres;Password=mysecretpassword;Host=localhost,localhost;Port=44217;Database=postgres;Pooling=true;LoadTableComposites=true; No Reset On Close=true;");
+
+await using var dataSource = dataSourceBuilder.BuildMultiHost();
+var ds = dataSource.WithTargetSession(TargetSessionAttributes.ReadOnly);
+
+using var log = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+
+log.Information("Hello, Serilog!");
+
+for (int i = 0; i < 1_000; i++)
+{
+    try
+    {
+        var conn = ds.CreateConnection();
+        //var connection = await dataSource.OpenConnectionAsync(TargetSessionAttributes.ReadWrite);
+        //var connection1 = await dataSource.OpenConnectionAsync();
+        var connection1 = await ds.OpenConnectionAsync();
+    }
+    catch (Exception e)
+    {
+        log.Error(e, "my error, failed to open connect");
+        Console.WriteLine(e);
+    }
+}
